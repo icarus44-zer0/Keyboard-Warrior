b3fccd9a427c7304ac583cdcb838a9996156905b
Josh Poe <42955626+icarus44-zer0@users.noreply.github.com>
2020-03-31 12:56:31 -0700

    initialized Native Key Listener

---
+++ src/main/Main.java
@@ -0,0 +1,48 @@
+package main;
+
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main implements NativeKeyListener {
+	
+	public static void main(String[] args) {
+		try {
+		GlobalScreen.registerNativeHook();
+		} catch (NativeHookException e ) {
+			e.printStackTrace();
+		}
+		
+		GlobalScreen.addNativeKeyListener(new Main());
+		
+		Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+		logger.setLevel(Level.OFF);
+
+		// Change the level for all handlers attached to the default logger.
+		Handler[] handlers = Logger.getLogger("").getHandlers();
+		for (int i = 0; i < handlers.length; i++) {
+			handlers[i].setLevel(Level.OFF);
+		}
+		
+	}
+	    @Override
+	    public void nativeKeyPressed(NativeKeyEvent e) {
+	    	System.out.print(NativeKeyEvent.getKeyText(e.getKeyCode()) + "" );
+	    }
+
+	    @Override
+	    public void nativeKeyReleased(NativeKeyEvent e) {
+	    	//System.out.println("KeyReleased " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+
+	    }
+
+	    @Override
+	    public void nativeKeyTyped(NativeKeyEvent e) {
+	    
+		
+	}
+}
