10e37ec87a54726413978ebb47f0d2044459ce6f
oscar <Vaqueraoscar0@gmail.com>
2020-04-01 15:17:30 -0700

    switch statement for key exceptions

---
+++ src/main/Tools.java
@@ -0,0 +1,50 @@
+package main;
+/*
+*   Usefult tools can be written here!
+*/
+public class Tools {
+    
+    
+    public static boolean containsLoggerKey(String key) {
+		switch(key) {
+		  case "Tab":
+				  return false;
+		  case "Space":
+				  return false;
+		  case "Shift":
+				return false;
+		  case "Return":
+				return false;
+		  case "Delete":
+				  return false;
+		  case "Backspace":
+				  return false;
+		  case "Enter":
+				return false;
+		  case "Ctrl":
+				return false;	
+		  case "Alt":
+				  return false;
+		  case "Meta":
+				  return false;
+		  case "Escape":
+				return false;
+		  case "Undefined":
+				return false;
+		  case "Left":
+				  return false;
+		  case "Down":
+				  return false;
+		  case "Right":
+				return false;
+		  case "Up":
+				return false;
+		  case "Unknown keyCode: 0xe36":
+				  return false;
+		  default:
+			return true;
+		}
+
+			
+	}
+}
