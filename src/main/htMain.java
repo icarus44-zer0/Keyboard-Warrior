67d2be1e8f1369cff357c3b6accd2256996f82fc
Josh Poe <42955626+icarus44-zer0@users.noreply.github.com>
2020-04-04 15:02:58 -0700

    created test main for HT filehandler

--- src/main/htMain.java
+++ src/main/htMain.java
@@ -1,9 +1,11 @@
 package main;
 import java.util.HashMap;
 
+
+
 public class htMain {
     public static void main(String[] args) {
-        hm_fileHandler htl = new hm_fileHandler();
+        hm_FileHandler htl = new hm_FileHandler();
         HashMap<String, String> hm1 = new HashMap<String, String>();
         
         hm1.put("key1", "value1");
@@ -14,14 +16,11 @@ public class htMain {
 
         htl.print(hm1);
         htl.hashMap_toFile(hm1);
+        System.out.println("Exported to file");
 
         HashMap<String, String> hm2 = new HashMap<String, String>();
-        System.out.println("Imported from file");
-        htl.print(hm2);
-
         hm2 = (HashMap<String, String>) htl.file_toHashMap(hm2);
-        System.out.println("Imported from file");
         htl.print(hm2);
-
+        System.out.println("Imported from file");
     }
 }
