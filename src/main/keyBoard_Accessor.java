b3202169c52d18c83e1b60c5318d2e70e9c48f44
oscar <Vaqueraoscar0@gmail.com>
2020-04-05 12:30:56 -0700

    modified key code formatter methods

--- src/main/keyBoard_Accessor.java
+++ src/main/keyBoard_Accessor.java
@@ -63,44 +63,147 @@ public class keyBoard_Accessor implements NativeKeyListener {
         return ret;
     }
 
-    private boolean isSpecialKey(String key) {
-        switch (key) {
-            case "Tab":
-                return false;
-            case "Space":
-                return false;
-            case "Shift":
-                return false;
-            case "Return":
-                return false;
-            case "Delete":
-                return false;
-            case "Backspace":
-                return false;
-            case "Enter":
-                return false;
-            case "Ctrl":
-                return false;
-            case "Alt":
-                return false;
-            case "Meta":
-                return false;
-            case "Escape":
-                return false;
-            case "Undefined":
-                return false;
-            case "Left":
-                return false;
-            case "Down":
-                return false;
-            case "Right":
-                return false;
-            case "Up":
-                return false;
-            case "Unknown keyCode: 0xe36":
-                return false;
-            default:
-                return true;
-        }
+    public static boolean isSpecialKey(String key, Buffer buf) {
+		isPunctuationKey(key);
+		
+		switch(key) {
+		  case "Tab":
+				  return false;
+		  case "Space":
+			  	buf.clear();
+				return false;
+		  case "Shift":
+				return false;
+		  case "Return":
+			  	return false;
+		  case "Delete":
+				return false;
+		  case "Backspace":
+				return false;
+		  case "Enter":
+			  	buf.clear();
+			  	return false;
+		  case "Ctrl":
+				return false;	
+		  case "Alt":
+				return false;
+		  case "Meta":
+				return false;
+		  case "Escape":
+				return false;
+		  case "Undefined":
+				return false;
+		  case "Left":
+				return false;
+		  case "Down":
+				return false;
+		  case "Right":
+				return false;
+		  case "Up":
+				return false;
+		  case "Unknown keyCode: 0xe36":
+				return false;
+		  case "Back Quote":
+			  	return false;
+		  case "Minus":
+			  	return false;
+		  case "Equals":
+			  	return false;
+		  case "Open Bracket":
+			  	return false;
+		  case "Close Bracket":
+			  	return false;
+		  case "Semicolon":
+			  	return false;
+		  case "Quote":
+			  	return false;
+		  case "Comma":
+			  	return false;
+		  case "Period":
+			  	return false;
+		  case "Slash":
+			  	return false;
+		  case "Back Slash":
+			  	return false;
+		  default:
+			  	return true;
+		}
+
+			
+	}
+	
+	
+	public static void isPunctuationKey(String key) {
+		switch(key) {
+		  case "Space":
+				System.out.print(" ");
+				break;
+		  case "Return":
+			  	System.out.println();
+				break;
+		  case "Enter":
+			  	System.out.println();
+			  	break;
+		  case "Tab":
+			  	System.out.print("	");
+			  	break;
+		  case "Back Quote":
+				System.out.print("`");
+				break;
+		  case "Minus":
+			  	System.out.print("-");
+				break;
+		  case "Equals":
+			  	System.out.print("=");
+			  	break;
+		  case "Open Bracket":
+			  	System.out.print("[");
+			  	break;
+		  case "Close Bracket":
+			  	System.out.print("]");
+				break;
+		  case "Semicolon":
+			  	System.out.print(";");
+			  	break;
+		  case "Quote":
+			  	System.out.print("'");
+			  	break;
+		  case "Comma":
+				System.out.print(",");
+				break;
+		  case "Period":
+			  	System.out.print(".");
+				break;
+		  case "Slash":
+			  	System.out.print("/");
+			  	break;
+		  case "Back Slash":
+			  	System.out.print("\\");
+			  		break;
+		  default:
+			  	return;
+		}
     }
+    
+    public static void textExpander(Buffer buf){
+		String temp = "";
+		
+		try {
+			textExpanderData();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Object[] arr = buf.toArray();
+		for(int i = 0; i < arr.length; i++) {
+			temp += arr[i];
+		}
+		
+		temp = temp.toLowerCase();
+		
+		if( m.containsKey(temp)) {
+			System.out.println(m.get(temp));
+		}
+	}
 }
