d00fc9f26c9595cf72a3574f26714f58e5ebe6b9
boomthegoat <boom916@outlook.com>
2020-04-08 15:52:31 -0700

    merged with clipboard accessor

---
+++ src/main/Clipboard_accessor.java
@@ -0,0 +1,30 @@
+package main;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.*;
+import java.io.IOException;
+
+public class Clipboard_accessor{
+    Clipboard_accessor(){}
+    public static String readClipboard() throws IOException{
+		Clipboard cb = Toolkit.getDefaultToolkit().getSystemClipboard();
+		Transferable t = cb.getContents(null);
+		try{
+			String data = (String) t.getTransferData(DataFlavor.stringFlavor);
+			return data;
+		}catch(UnsupportedFlavorException e) {}
+		return "";
+	}
+	public static void writeClipboard(String toClipbboard){
+		StringSelection stringSelection = new StringSelection(toClipbboard);
+		Clipboard cb = Toolkit.getDefaultToolkit().getSystemClipboard();
+		cb.setContents(stringSelection, null);
+	}
+}
+
+// try{
+//     System.out.println("Clipboard: " + readClipboard());
+// }catch (IOException e) {
+//     //TODO: handle exception
+// }
+// Clipboard_Accessor.writeClipboard("String goes here");
