6c35d769abd2446ab6d4796c62e3fe7e2522addd
Josh Poe <42955626+icarus44-zer0@users.noreply.github.com>
2020-04-16 11:04:46 -0700

    All features implemented - no GUI

--- src/com/purplecobras/keyboardwarrior/Main.java
+++ src/com/purplecobras/keyboardwarrior/Main.java
@@ -1,26 +1,39 @@
 package com.purplecobras.keyboardwarrior;
 
+import java.awt.Robot;
+import java.util.HashMap;
+
 public class Main {
 	public static final int MAX_BUFFER_SIZE = 10;
 
 	public static void main(String[] args) {
-		textExpanderInterface.printFrame();
+		// textExpanderInterface.printFrame();
 		Global_Keyboard_Listener listener = new Global_Keyboard_Listener();
+		Shortcut shortcut = new Shortcut();
 		Buffer_Search search = Buffer_Search.get_Instance();
+
 		
+		//will be romoved in new main class
+		HashMap<String,String> short_cut = new HashMap<String,String>();
+		short_cut = HashMap_File_Reader.hashMap_In(short_cut);
+
 		listener.setup();
 
-		String shortcut = "";
+		//Paste_debug();
 
 		while (true) {
 			KeyBoard_In_Buffer buffer = KeyBoard_In_Buffer.getBuffer();
 			try {
-				shortcut = search.search_BufferforKeys(buffer);
-				System.out.println(shortcut);
+				Robot robot = new Robot();
+				shortcut = search.bufferSearch(buffer);	
+				if (shortcut.get_scValue() != null) {
+					Clipboard_Accessor.writeClipboard(shortcut.get_scValue());
+					Insertion_Point_Accessor.delete_sckey(robot,shortcut.get_scKey());
+					Insertion_Point_Accessor.paste_scKey(robot);
+				}
 			} catch (Exception e) {
-				//System.out.println(e + "");
+				// System.out.println(e + "");
 			}
 		}
 	}
 }
-
