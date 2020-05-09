e39f2305bd7e7c4139812e62c7626980a231e235
icarus44-zer0 <joshuapoe219@gmail.com>
2020-05-08 23:16:17 -0700

    limiting buffer search loop - (non functonal)

---
+++ src/com/purplecobras/keyboardwarrior/Expand_Shortcut.java
@@ -0,0 +1,38 @@
+package com.purplecobras.keyboardwarrior;
+
+import java.awt.Robot;
+import java.util.concurrent.TimeUnit;
+
+public class Expand_Shortcut {
+    private static Expand_Shortcut instance = null;
+    private Buffer_Search search = Buffer_Search.get_Instance();
+    private KeyBoard_In_Buffer buffer = KeyBoard_In_Buffer.getInstance();
+    private Shortcut shortcut = new Shortcut();
+    private Robot robot;
+
+    private Expand_Shortcut() {
+
+    }
+
+    public static Expand_Shortcut getInstance() {
+        if (instance == null) {
+            instance = new Expand_Shortcut();
+        }
+        return instance;
+    }
+
+    public void expand() {
+        try {
+            shortcut = search.search_KBI_Buffer(buffer);
+            if (shortcut.get_Value() != null) {
+                robot = new Robot();
+                Clipboard_Accessor.writeClipboard(shortcut.get_Value());
+                Insertion_Point_Accessor.delete_sckey(robot, shortcut.get_Key());
+                Insertion_Point_Accessor.paste_Shortcut_Value(robot);
+            }
+            TimeUnit.MILLISECONDS.sleep(100);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
