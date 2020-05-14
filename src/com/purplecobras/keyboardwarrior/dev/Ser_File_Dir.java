8b8fd6eb9a767025726fd78602df61805ffd4265
icarus44-zer0 <joshuapoe219@gmail.com>
2020-05-13 22:24:51 -0700

    update SER file path

--- src/com/purplecobras/keyboardwarrior/dev/Ser_File_Dir.java
+++ src/com/purplecobras/keyboardwarrior/dev/Ser_File_Dir.java
@@ -10,7 +10,9 @@ public final class Ser_File_Dir {
     public static final String SF2 = "shiftkey.ser";
     public static final String SF3 = "commandkey.ser";
     public static final String SF4 = "deadkey.ser";
-    public static final String FILE_PATH = "src/com/purplecobras/keyboardwarrior/ser";
+    public static final String FILE_PATH = "src/com/purplecobras/keyboardwarrior/ser/";
+
+
     
     private Ser_File_Dir(){
 
