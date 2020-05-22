917dec974fa447b1f4f37ffd26f95f475e45431a
icarus44_zer0 <42955626+icarus44-zer0@users.noreply.github.com>
2020-05-22 16:30:58 -0700

    removed all unused sef file imports

--- src/main/java/keyboardwarrior/SerFileDir.java
+++ src/main/java/keyboardwarrior/SerFileDir.java
@@ -3,7 +3,7 @@ package keyboardwarrior;
 
 public final class SerFileDir {
 
-    public static final String SF0 = "shortCut.ser";
+    public static final String FILE_NAME = "shortCut.ser";
     public static final String FILE_PATH = "src/main/java/keyboardwarrior/ser/";
     public static final String ICON_PATH = "src/main/java/keyboardwarrior/ico/";
 
